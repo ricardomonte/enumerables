a79690e080e13424b811f3c4a0a3eea9f278c977
ricardo montenegro <ramn21@gmail.com>
2020-09-25 22:38:43 +0200

    fix changes require

--- enumerable.rb
+++ enumerable.rb
@@ -1,5 +1,6 @@
+# rubocop:disable all
 module Enumerable
-  def my_each
+  def my_each()
     return to_enum(:my_each) unless block_given?
 
     var = to_a
@@ -9,19 +10,19 @@ module Enumerable
       result << yield(var[i])
       i += 1
     end
-    result
+    self
   end
 
-  def my_each_with_index
+  def my_each_with_index()
     return to_enum(:my_each_with_index) unless block_given?
 
     var = to_a
-    result = i = 0
+    i = 0
     while i < var.length
       result = yield(var[i], i)
       i += 1
     end
-    result
+    self
   end
 
   def my_select
@@ -32,85 +33,143 @@ module Enumerable
     arr
   end
 
-  def my_all?
+  def my_all?(arg = nil)
     var = to_a
-    var.empty?
-    if block_given?
-      my_each { |i| return false unless yield(i) }
-      return true
+    result = true
+    var.my_each do |n|
+      if block_given?
+        if yield(n)
+          result = true
+        elsif !yield(n)
+          result = false
+        end
+      elsif arg.nil?
+        result = false if n.nil? || n == false
+      else
+        if arg === n
+          result = false
+          break
+        end
+      end
     end
-    var.include?(nil) || var.include?(false) ? false : true
+    result
   end
 
-  def my_any?
+  def my_any?(arg = nil)
     var = to_a
-    if block_given?
-      var.my_each { |i| return true if yield(i) }
-    else
-      var.my_each { |i| return true if i.nil? == false and i != false }
+    result = true
+    var.my_each do |n|
+      if block_given?
+        if yield(n)
+          result = true
+        elsif !yield(n)
+          result = false
+        end
+      elsif arg.nil?
+        result = false if n.nil? || n == false
+      else
+        unless arg === n
+          result = false
+          break
+        end
+      end
     end
-    false
+    result
   end
 
-  def my_none?
-    var = to_a
-    if block_given?
-      var.my_each { |i| return false if yield(i) }
-    else
-      var.my_each { |i| return false if i.nil? == false and i != false }
+  def my_none?(arg = nil)
+    result = true
+    my_each do |n|
+      if block_given?
+        if yield(n)
+          result = false
+          break
+        elsif !yield(n)
+          result = true
+        end
+      elsif arg.nil?
+        result = false if n == true
+      else
+        if arg === n
+          result = false
+          break
+        end
+      end
     end
-    true
+    result
   end
 
   def my_count(*arg)
+    var = to_a
     sum = 0
     if block_given?
-      my_each { |i| sum += 1 if yield(i) }
+      var.my_each { |i| sum += 1 if yield(i) }
       sum
     elsif arg.empty?
-      length
+      var.length
     else
-      my_each { |i| sum += 1 if arg[0] == i }
+      var.my_each { |i| sum += 1 if arg[0] == i }
       sum
     end
   end
 
   def my_map(arg = nil)
+    var = to_a
     arr = []
     if arg
-      my_each { |i| arr.push(arg.call(i)) }
+      var.my_each { |i| arr.push(arg.call(i)) }
       return arr
     end
-    return unless block_given?
+    return to_enum(:my_map) unless block_given?
 
-    my_each { |i| arr.push(yield(i)) }
+    var.my_each { |i| arr.push(yield(i)) }
     arr
   end
 
   def my_inject(*arg)
     memo = 0
+    var = to_a
+    if var.my_all? { |x| x.is_a? String }
+      memo = ''
+      var.my_each { |i| memo = yield(memo, i) }
+      return memo
+    end
     if arg.empty?
-      my_each { |i| memo = yield(memo, i) }
+      var.my_each { |i| memo = yield(memo, i) }
       return memo
     end
     if arg.empty? == false && block_given?
-      my_each do |i|
+      var.my_each do |i|
         memo = yield(arg[0], i)
         arg[0] = memo
       end
       return memo
     end
     if arg[0].is_a? Symbol
-      my_each { |i| memo = memo.send(arg[0], i) }
-      return memo
+      case arg[0]
+      when :+ || :-
+        memo = 0
+        my_each do |i|
+          memo = memo.send(arg[0], i)
+        end
+        return memo
+      when :* || :/
+        memo = 1
+        my_each do |i|
+          memo = memo.send(arg[0], i)
+        end
+        return memo
+      end
     end
     return unless arg.length == 2
 
-    my_each { |i| memo = arg[0] = arg[0].send(arg[1], i) }
+    var.my_each { |i| memo = arg[0] = arg[0].send(arg[1], i) }
     memo
   end
+end
 
-  def multiply_els
-    my_inject(1, :*)
-  end
+# rubocop: enable all
+
+def multiply_els(arg)
+  arg.my_inject(:+)
 end
